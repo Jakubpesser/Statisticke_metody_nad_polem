c317e907772eb2a7e377081f507c38a53595def6
jakub.pesser.022 <jakub.pesser.022@pslib.cz>
2023-10-06 11:31:34 +0200

    vec

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿int[] pole = {22, 56, 45, 2, 12, 3};
+
+Console.WriteLine(pole.Length);
+
+Console.WriteLine(pole.Max());
+
+Console.WriteLine(pole.Min());
+
+double FinalSum = 0;
+for (int i = 0; i < pole.Length; i++)
+{
+    FinalSum += pole[i];
+}
+Console.WriteLine(FinalSum);
+
+double FinalProduct = 1;
+for (int i = 0; i < pole.Length; i++)
+{
+    FinalProduct *= pole[i];
+}
+Console.WriteLine(FinalProduct);
+
+double AritmetickýPrůměr = FinalSum / pole.Length;
+Console.WriteLine(AritmetickýPrůměr);
+
+double GeometrickýPrůměr = Math.Pow(FinalProduct, 1d / pole.Length);
+Console.WriteLine(GeometrickýPrůměr);
+
+double Median(int[] pole)
+{
+    Array.Sort(pole);
+    if (pole.Length % 2 != 0)
+    {
+        return pole[pole.Length / 2];
+    }
+    double Větší = pole[(pole.Length - 0) / 2]; //22
+    double Menší = pole[(pole.Length - 2) / 2]; //12
+    double T = (Větší + Menší) / 2;
+    return T;
+}
+Console.WriteLine(Median(pole));
+Console.ReadLine();
+
---
+++ ConsoleApp2/Zadání.cs
@@ -0,0 +1,25 @@
+﻿int[] pole = { 12, 5, 58 };
+Console.WriteLine(pole[0]);
+pole[1] = 8;
+Console.WriteLine(pole[1]);
+
+int delka = pole.Length;
+Console.WriteLine(delka);
+Array.Sort(pole); //řazení pole
+for (int i = 0; i < delka; i++)
+{
+    Console.WriteLine(pole[i]);
+}
+
+Console.WriteLine("výpis pomocí foreach");
+foreach (int prvek in pole)
+{
+    Console.WriteLine(prvek);
+    Console.WriteLine(pole.Contains(0));
+    Console.WriteLine(pole.Contains(58));
+}
+
+
+Console.ReadLine();
+
+
